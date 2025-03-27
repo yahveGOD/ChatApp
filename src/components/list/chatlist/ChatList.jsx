51c12b599ac6f61d3a8b09f07d952cb8bfb41975
yahveGOD <savinskytop1@gmail.com>
2025-03-27 16:39:09 +0300

    finishing front-end dev

--- src/components/list/chatlist/ChatList.jsx
+++ src/components/list/chatlist/ChatList.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import "./chatList.css"
+import AddUser from "./addUser/AddUser"
 
 const ChatList = () => {
     const[addMode,setAddMode]= useState(false)
@@ -70,7 +71,7 @@ const ChatList = () => {
             <p>Message</p>
           </div>
         </div>
-
+        {addMode && <AddUser/>}
       </div>
     )
   }
